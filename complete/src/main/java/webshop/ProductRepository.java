c20d05df7807c9f04a505987a67c11a0061e11c7
ChrilleJigling <Christianjigling@hotmail.com>
2020-01-09 15:51:08 +0100

    Added html pages for all categorys

--- complete/src/main/java/webshop/ProductRepository.java
+++ complete/src/main/java/webshop/ProductRepository.java
@@ -1,18 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package webshop;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- *
- * @author melin
- */
+
 public interface ProductRepository extends JpaRepository<Product, Integer>{
     
     public List<Product> findByNameLike(String name);
+    
+    public List<Product> findByCategory(String category);
 }
