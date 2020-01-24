18f0d7bc4843061051ce95b7abdaa6c956909762
ChrilleJigling <43202652+ChrilleJigling@users.noreply.github.com>
2020-01-24 13:04:00 +0100

    --

--- complete/src/test/java/jUnit/webshopServiceTest.java
+++ complete/src/test/java/jUnit/webshopServiceTest.java
@@ -4,9 +4,12 @@ package jUnit;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import webshop.WebshopService;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class webshopServiceTest {
     
     @Autowired
